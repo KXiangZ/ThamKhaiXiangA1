a23965af91e4d2cafed07ad38b46a53a7af68512
KXiangZ <freakclonetkx9946@gmail.com>
2016-04-14 12:13:09 +0800

    Initial commit

---
+++ ThamKhaiXiangA1.py
@@ -0,0 +1,28 @@
+"""
+Tham Khai Xiang 14/4/2016
+This is a program taht allow for hiring or returning multiple item.
+github link:
+"""
+
+def loading_item():
+
+
+
+
+def hiring_item():
+
+#Welcome message
+print('Items for Hire - by Tham Khai Xiang')
+
+
+
+
+
+
+3 items loaded from items.csv
+Menu:
+(L)ist all items
+(H)ire an item
+(R)eturn an item
+(A)dd new item to stock
+(Q)uit
