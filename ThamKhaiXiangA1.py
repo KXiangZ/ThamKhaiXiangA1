21686d050b7bf931fa62709e7f0297e52fa89d0a
KXiangZ <freakclonetkx9946@gmail.com>
2016-04-20 18:45:08 +0800

    add_item function added

--- ThamKhaiXiangA1.py
+++ ThamKhaiXiangA1.py
@@ -9,19 +9,18 @@ github link:https://github.com/KXiangZ/ThamKhaiXiangA1
 print('Items for Hire - by Tham Khai Xiang')
 
 
-
 def load_item():
-    show=''
-    counter=0
+    show = ''
+    counter = 0
     input_file = open('inventory.csv', 'r+')
     for data in input_file:
         storage = data.split(',')
         storage[-1] = storage[-1].strip()
-        if storage[3]=="in":
-            storage[3]=""
-        elif storage[3]=="out":
-            storage[3]="*"
-        show+=("{} - {}({})\t\t\t\t= $ {:.2f} {}\n ".format(counter,storage[0],storage[1],float(storage[2]),storage[3]))
+        if storage[3] == "in":
+            storage[3] = ""
+        elif storage[3] == "out":
+            storage[3] = "*"
+        show+=("{:3} - {}({}){:20}= $ {:.2f} {}\n ".format(counter,storage[0],storage[1],"",float(storage[2]),storage[3]))
         counter+=1
     return (show)
 
@@ -40,7 +39,7 @@ def hire_item():
         elif storage[3] == "out":
             storage[3] = "*"
         if storage[3]=="":
-            show += ("{} - {}({})\t\t\t\t= $ {:.2f} {}\n ".format(counter, storage[0], storage[1], float(storage[2]), storage[3]))
+            show += ("{:3} - {}({}){:30}= $ {:.2f} {}\n ".format(counter, storage[0], storage[1],"", float(storage[2]), storage[3]))
             if record=="":
                 record+=str(counter)
             else:
@@ -76,7 +75,7 @@ def return_item():
         elif storage[3] == "out":
             storage[3] = "*"
         if storage[3] == "*":
-            show += ("{} - {}({})\t\t\t\t= $ {:.2f} {}\n ".format(counter, storage[0], storage[1], float(storage[2]),storage[3]))
+            show += ("{:3} - {}({}){:30}= $ {:.2f} {}\n ".format(counter, storage[0], storage[1],"", float(storage[2]),storage[3]))
             if record == "":
                 record += str(counter)
             else:
@@ -98,6 +97,15 @@ def return_item():
                 num += 1
             print(ret)
             input_file.close()
+
+def add_item():
+    name_input=input("Item name: ")
+    description_input=input("Description: ")
+    price_input=input("Price per day: ")
+    input_file = open('inventory.csv', 'a+')
+    input_file.write("\n{},{},{},in".format(name_input,description_input,price_input))
+    print("{}({}),${} now available for hire".format(name_input,description_input,price_input))
+    input_file.close()
 #3 items loaded from items.csv
 #user_input=input("""Menu:
 #(L)ist all items
@@ -105,4 +113,4 @@ def return_item():
 #(R)eturn an item
 #(A)dd new item to stock
 #(Q)uit""")
-print (return_item())
+print (load_item())
